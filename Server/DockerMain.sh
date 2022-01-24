d26bb52e37dd92307fbcc89dc719ad1227603519
Mark <mark.gilbert@m4kr.net>
2022-01-24 05:33:26 -0600

    Update DockerMain.sh

--- Server/DockerMain.sh
+++ Server/DockerMain.sh
@@ -8,11 +8,11 @@ RemotelyData=/remotely-data
 AppSettingsVolume=/remotely-data/appsettings.json
 AppSettingsWww=/var/www/remotely/appsettings.json
 
-ls -ltrah /var/www/remotely/Remotely_Server_Installer
+ls -ltrah /var/www/Remotely_Server_Installer
 
 if [ "$BUILD" = true ]; then
   echo "Building from source"
-  cd /var/www/remotely && ./Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c true -s $URL -i /var/www/remotely  -w 0 -r $REFERENCE
+  cd /var/www && ./Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c true -s $URL -i /var/www/remotely  -w 0 -r $REFERENCE
 fi
 
 sed -i 's/DataSource=Remotely.db/DataSource=\/remotely-data\/Remotely.db/' /var/www/remotely/appsettings.json
