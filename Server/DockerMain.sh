b0a4dbfbb3da16a26a5f4d57b3c083166ebab4b2
Mark <mark.gilbert@m4kr.net>
2022-01-24 05:48:21 -0600

    Update DockerMain.sh

--- Server/DockerMain.sh
+++ Server/DockerMain.sh
@@ -8,18 +8,14 @@ RemotelyData=/remotely-data
 AppSettingsVolume=/remotely-data/appsettings.json
 AppSettingsWww=/var/www/remotely/appsettings.json
 
-ls -ltrah /var/www/Remotely_Server_Installer
+wget -q https://github.com/lucent-sea/Remotely/releases/latest/download/Remotely_Server_Linux-x64.zip 
+unzip -o Remotely_Server_Linux-x64.zip -d /var/www/remotely 
+rm Remotely_Server_Linux-x64.zip
 
 if [ "$BUILD" = true ]; then
   echo "Building from source"
-  cd /var/www && ./Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c true -s $URL -i /var/www/remotely  -w 0 -r $REFERENCE
-  
-  
+  cd /var/www && ./Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c true -s $URL -i /var/www/remotely  -w 0 -r $REFERENCE 
 fi
-
- wget -q https://github.com/lucent-sea/Remotely/releases/latest/download/Remotely_Server_Linux-x64.zip && \
-  unzip -o Remotely_Server_Linux-x64.zip -d /var/www/remotely && \
-  rm Remotely_Server_Linux-x64.zip
 
 sed -i 's/DataSource=Remotely.db/DataSource=\/remotely-data\/Remotely.db/' /var/www/remotely/appsettings.json
 
