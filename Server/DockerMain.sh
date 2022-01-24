72c8ac9a11cda34118e02e96d9e2a25df700f12e
Mark <mark.gilbert@m4kr.net>
2022-01-24 06:49:50 -0600

    Update DockerMain.sh

--- Server/DockerMain.sh
+++ Server/DockerMain.sh
@@ -8,16 +8,9 @@ RemotelyData=/remotely-data
 AppSettingsVolume=/remotely-data/appsettings.json
 AppSettingsWww=/var/www/remotely/appsettings.json
 
-if [ "$BUILD" = true ]; then
-  echo "Source Server Files"
-  wget -q https://github.com/lucent-sea/Remotely/releases/latest/download/Remotely_Server_Linux-x64.zip 
-  unzip -o Remotely_Server_Linux-x64.zip -d /var/www/remotely 
-  rm Remotely_Server_Linux-x64.zip
-  
-  echo "Building from source"
-  cd /var/www && ./Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c true -s $URL -i /var/www/remotely  -w 1 -r $REFERENCE 
-fi
-cd /var/www/remotely
+echo "Building from source: $BUILD"
+/var/www/Remotely_Server_Installer -b false -u $GITUSER -p $KEY -c $BUILD -s $URL -i /var/www/remotely  -w 1 -r $REFERENCE 
+
 sed -i 's/DataSource=Remotely.db/DataSource=\/remotely-data\/Remotely.db/' /var/www/remotely/appsettings.json
 
 if [ ! -f "$AppSettingsVolume" ]; then
